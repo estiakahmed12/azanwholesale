83ec80e84d37cd9968a2db0de6c9a37210a88b62
estiakahmed12 <estiakahmed898@gmail.com>
2024-11-12 15:01:10 +0600

    button Updatated

--- app/page.js
+++ app/page.js
@@ -1,5 +1,26 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 const HomePage = () => {
-  return <div className="text-2xl text-center mt-8">Home Page</div>;
+  const router = useRouter();
+
+  const handleLoginClick = () => {
+    router.push("/auth/login");
+  };
+
+  return (
+    <div className="text-2xl text-center mt-8">
+      <h1>Home Page</h1>
+
+      <button
+        type="button"
+        onClick={handleLoginClick}
+        className="mt-10 py-2 px-4 w-52 bg-[#F03C7D] text-white rounded-xl font-medium hover:bg-[#F03C7D]/85"
+      >
+        Log In
+      </button>
+    </div>
+  );
 };
 
 export default HomePage;
